8d3033cda956e633ac4186e95e1b01ac787cbf87
Gustavo <gtlcouto@gmail.com>
2016-05-02 14:50:05 -0400

    Initial Commit

---
+++ W1D1WordPlay/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  W1D1WordPlay
+//
+//  Created by Gustavo Couto on 2016-05-02.
+//  Copyright © 2016 Gustavo Couto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
